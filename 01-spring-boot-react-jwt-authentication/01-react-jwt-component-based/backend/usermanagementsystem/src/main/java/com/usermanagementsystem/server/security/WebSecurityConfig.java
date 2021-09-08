0cdc1dfb62567d7a8471f7f2d5cde170ac541a44
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-09-08 22:53:00 +0200

    Task 12: create spring restapis test role controller

--- 01-spring-boot-react-jwt-authentication/01-react-jwt-component-based/backend/usermanagementsystem/src/main/java/com/usermanagementsystem/server/security/WebSecurityConfig.java
+++ 01-spring-boot-react-jwt-authentication/01-react-jwt-component-based/backend/usermanagementsystem/src/main/java/com/usermanagementsystem/server/security/WebSecurityConfig.java
@@ -56,8 +56,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/api/authentication/**").permitAll()
-                .antMatchers("/api/authentication/**").permitAll()
+                .authorizeRequests().antMatchers("/authentication/**").permitAll()
+                .antMatchers("/authentication/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
