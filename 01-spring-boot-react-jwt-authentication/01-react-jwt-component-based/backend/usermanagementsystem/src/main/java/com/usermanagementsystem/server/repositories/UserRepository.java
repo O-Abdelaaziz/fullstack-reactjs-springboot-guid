7e6285fc57b21897d2242f84473c97ddcf882a1f
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-09-08 19:15:54 +0200

    Task 4: implement repositories (user-repository, role-repository)

---
+++ 01-spring-boot-react-jwt-authentication/01-react-jwt-component-based/backend/usermanagementsystem/src/main/java/com/usermanagementsystem/server/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.usermanagementsystem.server.repositories;
+
+import com.usermanagementsystem.server.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
