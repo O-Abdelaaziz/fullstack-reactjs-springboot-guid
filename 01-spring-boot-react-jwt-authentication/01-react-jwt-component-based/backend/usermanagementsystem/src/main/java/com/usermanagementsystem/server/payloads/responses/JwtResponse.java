96dbe1db4ff756b1504d354e5676b85c2983cbfe
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-09-08 20:28:45 +0200

    Task 10: define payloads for spring restcontroller

---
+++ 01-spring-boot-react-jwt-authentication/01-react-jwt-component-based/backend/usermanagementsystem/src/main/java/com/usermanagementsystem/server/payloads/responses/JwtResponse.java
@@ -0,0 +1,21 @@
+package com.usermanagementsystem.server.payloads.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private String uid;
+    private String username;
+    private String email;
+    private Boolean isEnabled;
+    private Boolean isNotLocked;
+    private List<String> roles;
+}
