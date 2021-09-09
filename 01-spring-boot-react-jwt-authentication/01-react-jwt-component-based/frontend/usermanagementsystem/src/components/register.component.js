7a546c5c2eda28aeaa99a7ba85789d7e507d9ded
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-09-09 10:01:45 +0200

    Task 20: create react components for authentication

---
+++ 01-spring-boot-react-jwt-authentication/01-react-jwt-component-based/frontend/usermanagementsystem/src/components/register.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class RegisterComponent extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default RegisterComponent;
