f932a581e60979ff6c94727cf053f1bb80382589
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-09-09 13:59:32 +0200

    Task 25: create react components for accessing resources

---
+++ 01-spring-boot-react-jwt-authentication/01-react-jwt-component-based/frontend/usermanagementsystem/src/components/board-user.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class BoardUserComponent extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default BoardUserComponent;
