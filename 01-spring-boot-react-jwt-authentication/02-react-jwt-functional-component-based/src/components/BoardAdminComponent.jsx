64e89de8290b73bfbdd2eaaffed64da4a2a26ddf
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-09-19 16:08:22 +0200

    Task 46: working with admin-page

---
+++ 01-spring-boot-react-jwt-authentication/02-react-jwt-functional-component-based/src/components/BoardAdminComponent.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import authenticationUserService from '../services/authentication.user.service';
+
+const BoardAdminComponent = () => {
+    const [content, setContent] = useState("");
+
+    useEffect(() => {
+      authenticationUserService.getAdminModeratorContent().then(
+        (response) => {
+          setContent(response.data);
+        },
+        (error) => {
+          const _content =
+            (error.response && error.response.data) ||
+            error.message ||
+            error.toString();
+  
+          setContent(_content);
+        }
+      );
+    }, []);
+  
+    return (
+      <div className="container">
+        <header className="jumbotron">
+          <h3>{content}</h3>
+        </header>
+      </div>
+    );
+}
+
+export default BoardAdminComponent
